bd0ef64a6dead8a32b37a2bff82fa2f6d1063b5c
goelaryan001 <goel.aryan001@fmail.com>
2025-12-30 08:52:32 -0800

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,8 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 '''
 Whenever we are performing the data ingestion component, there should be some inputs that may be probably
@@ -77,4 +79,6 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_transormation=DataTransformation()
+    data_transormation.initiate_data_transformation(train_data,test_data)
