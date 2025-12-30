42d9229d121af2719ff15423ca49be06d4ff670a
goelaryan001 <goel.aryan001@fmail.com>
2025-12-30 10:39:26 -0800

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
 
 '''
 Whenever we are performing the data ingestion component, there should be some inputs that may be probably
@@ -81,4 +83,7 @@ if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
     data_transormation=DataTransformation()
-    data_transormation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transormation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
